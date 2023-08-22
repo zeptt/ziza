20116eb4906927a80e95f106a6c62d10fc7f1637
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2023-08-22 19:21:46 +0530

    added testing playground

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { POST } from "./server";
+
 export const Hello = (s: string) => {
   if (!s) {
     throw new Error("No name provided");
   }
   return `Hello ${s}!`;
 };
+
+export { POST };
--- src/server/email.ts
+++ src/server/email.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
 export function POST(req: NextApiRequest, res: NextApiResponse) {
+  NextResponse.redirect("/");
   return res.status(200).json({ message: "Email sent" });
 }
