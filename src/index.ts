0b74405d9512b4259aa02f2919b6e5e06acf3706
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2023-08-25 22:22:33 +0530

    fixed CI/CD

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { POST } from "./server";
-
-export const Hello = (s: string) => {
-  if (!s) {
-    throw new Error("No name provided");
-  }
-  return `Hello ${s}!`;
-};
-
-export { POST };
+export * from "./server";
