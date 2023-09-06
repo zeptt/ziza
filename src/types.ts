f5064aaebe84256e97e52241fee34d136b38ef73
Kushagra102 <kushagra.garg77@gmail.com>
2023-09-07 00:26:39 +0530

    Attachment types Added

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,50 @@
 import { z } from "zod";
 
+const HeadersSchema = z.union([
+  z.record(
+    z.union([
+      z.string(),
+      z.array(z.string()),
+      z.object({ prepared: z.boolean(), value: z.string() }),
+    ])
+  ),
+  z.array(z.object({ key: z.string(), value: z.string() })),
+]);
+
+const AttachmentLikeSchema = z.object({
+  content: z.union([z.string(), z.instanceof(Buffer)]).optional(),
+  path: z.string().optional(),
+});
+
+const AttachmentSchema = AttachmentLikeSchema.merge(
+  z.object({
+    filename: z.union([z.string(), z.literal(false)]).optional(),
+    cid: z.string().optional(),
+    encoding: z.string().optional(),
+    contentType: z.string().optional(),
+    contentTransferEncoding: z
+      .union([
+        z.literal("7bit"),
+        z.literal("base64"),
+        z.literal("quoted-printable"),
+        z.literal(false),
+      ])
+      .optional(),
+    contentDisposition: z
+      .union([z.literal("attachment"), z.literal("inline")])
+      .optional(),
+    headers: HeadersSchema.optional(),
+    raw: z
+      .union([z.string(), z.instanceof(Buffer), AttachmentLikeSchema])
+      .optional(),
+  })
+);
+
 export const EmailOptionsSchema = z.object({
   from: z.string().optional(),
   to: z.string().optional(),
   subject: z.string().optional(),
-  attachments: z.array(z.object({})).optional(),
+  attachments: z.array(AttachmentSchema).optional(),
   cc: z.string().optional(),
   bcc: z.string().optional(),
   replyTo: z.string().optional(),
